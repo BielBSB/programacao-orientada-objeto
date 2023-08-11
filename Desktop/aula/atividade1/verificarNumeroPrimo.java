be0b4b89d6e6650f1b75edf1b2d9a31b0acdaa99
BielDuCalote <gabrieleduardo2004@gmail.com>
2023-08-11 10:40:39 -0300

    atividade 1

---
+++ Desktop/aula/atividade1/verificarNumeroPrimo.java
@@ -0,0 +1,10 @@
+/**
+ * verificarNumeroPrimo
+ */
+public class verificarNumeroPrimo {
+
+    public static void main(String args[]) {
+        System.out.println("Ola, mundo!");        
+    }
+    
+}

46995cabefdfe753d6e737ab900491dbe1601868
Gabriel Eduardo Pereira <106703450+BielBSB@users.noreply.github.com>
2023-08-17 23:14:59 -0300

    verificaNumeroPrimo.java

--- Desktop/aula/atividade1/verificarNumeroPrimo.java
+++ Desktop/aula/atividade1/verificarNumeroPrimo.java
@@ -1,10 +1,28 @@
-/**
- * verificarNumeroPrimo
- */
-public class verificarNumeroPrimo {
+public class VerificadorPrimo {
 
-    public static void main(String args[]) {
-        System.out.println("Ola, mundo!");        
+    public static int verificaNumeroPrimo(Integer n) {
+        if (n <= 1) {
+            return 0; 
+        }
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0; 
+            }
+        }
+        
+    return 1; 
     }
-    
+
+    public static void main(String[] args) {
+        int numero = 23; // Insira aqui o número que deseja verificar
+        
+        int resultado = verificaNumeroPrimo(numero);
+        
+        if (resultado == 1) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+    }
 }
